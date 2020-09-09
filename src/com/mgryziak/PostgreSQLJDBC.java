8ca6beded2eadb1cffcf66746d4d5b21458e7026
Gosia Gryziak <gosiagryziak7606@gmail.com>
2020-09-09 10:46:06 +0200

    Metoda wywołująca wszystkie zapytania select

--- src/com/mgryziak/PostgreSQLJDBC.java
+++ src/com/mgryziak/PostgreSQLJDBC.java
@@ -2,10 +2,11 @@ package com.mgryziak;
 
 import com.mgryziak.DBqueries.PlayerInsert;
 import com.mgryziak.DBqueries.PlayerSelect;
+import com.mgryziak.DBqueries.PlayerUpdate;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
-
 
 public class PostgreSQLJDBC {
     public static void main(String args[]) {
@@ -15,14 +16,18 @@ public class PostgreSQLJDBC {
             c = DriverManager
                     .getConnection("jdbc:postgresql://localhost:5432/kluby",
                             "postgres", "qwerty2462");
+
             c.setAutoCommit(false);
-            PlayerSelect.ResultOfAllQueries(c);
+            PlayerSelect.ResultOfAllSelect(c);
             PlayerInsert.AllClubsInsert(c);
             PlayerInsert.AllSupport(c);
-            //PlayerInsert.InsertClubsSupport(c,5,11, Date.valueOf("2019-10-23"),45350);
+            PlayerInsert.InsertClubsSupport(c, 5, 11, Date.valueOf("2019-10-23"), 45350);
+            PlayerUpdate.updateExpenses(c, 78100.00, 3);
+            PlayerUpdate.updateSurname(c, "Zielińska", 10);
+
         } catch (Exception e) {
             e.printStackTrace();
-            System.err.println(e.getClass().getName()+": "+e.getMessage());
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
         }
     }
